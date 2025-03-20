ac62717519bd19cba38cc494bb3040c56efa4e0e
J4ninja <j4nguyen134@gmail.com>
2025-03-20 14:20:12 -0500

    add base templates and restructure

--- book_recommender_app/urls.py
+++ book_recommender_app/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('test_data_models', views.test_data_models, name = 'test_data_models')
+    path('search', views.test_data_models, name='search')  # Add this line
 ]
